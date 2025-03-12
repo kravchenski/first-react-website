b7ff3e272d75d9f97e18fd9e29adcd68abcb6a5c
kravchenski <max5045955@gmail.com>
2025-03-12 01:04:41 +0100

    init: first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import '../../App.css'
+import Cards from '../Cards';
+import HeroSection from '../HeroSection'
+import Footer from '../Footer';
+function Home() {
+    return (<>
+        <HeroSection />
+        <Cards />
+        <Footer></Footer>
+    </>)
+}
+export default Home;
